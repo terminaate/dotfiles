ddee41b7fce9c9bc8e7fe8d79329b3a4a28b8414
terminaate <terminaatecorp@gmail.com>
2025-05-03 06:45:14 +0300

    enable git-lfs in git config

--- hm/git.nix
+++ hm/git.nix
@@ -3,6 +3,7 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     userName = "terminaate";
     userEmail = "terminaatecorp@gmail.com";
     extraConfig.credential.helper = "manager";
