bd06dd8f2fcaea807f05be62cff8510cebe53450
terminaate <terminaatecorp@gmail.com>
2025-05-04 07:46:24 +0300

    refactor de.nix

--- nixos/modules/de.nix
+++ nixos/modules/de.nix
@@ -1,9 +1,15 @@
 { ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      gdm.enable = true;
+    };
+    desktopManager = {
+      gnome.enable = true;
+    };
+  };
 
   services.xserver.xkb = {
     layout = "us";
